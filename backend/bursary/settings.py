7e3ead57a282673f49aeaf5b444adad1340ff3c0
Melusi Ndlovu <melusindlovuc5@gmail.com>
2026-03-06 01:40:37 +0200

    fix the procfile and wsgi configs

--- backend/bursary/settings.py
+++ backend/bursary/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = "bursary.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
