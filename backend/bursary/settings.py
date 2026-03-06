6326451566053cb5b00c7182daefb9084761aaa0
Melusi Ndlovu <melusindlovuc5@gmail.com>
2026-03-07 01:12:59 +0200

    Fixed the static files structure to be found

--- backend/bursary/settings.py
+++ backend/bursary/settings.py
@@ -70,6 +70,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    ROOT_DIR / "static",
+]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = ROOT_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
