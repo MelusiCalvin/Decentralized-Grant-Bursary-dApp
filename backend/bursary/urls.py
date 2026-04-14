0987d8d88bd8fcdd907d0add5d788be04ae00607
Melusi Ndlovu <melusindlovuc5@gmail.com>
2026-04-14 22:41:04 +0200

    fixing the viewing of documents

--- backend/bursary/urls.py
+++ backend/bursary/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as media_serve
 from grants.urls import home
 
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path('', home, name='home'),   # homepage
     path("admin/", admin.site.urls),
     path("api/", include("grants.urls")),
+    # Serve uploaded application evidence files (PDFs) in production too.
+    re_path(r"^media/(?P<path>.*)$", media_serve, {"document_root": settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
